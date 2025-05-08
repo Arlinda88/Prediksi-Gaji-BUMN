b9597f44564cbe404a9d3b6aa806ce996711dd40
Arlinda88 <arlindasukmadewi7@gmail.com>
2025-05-08 09:49:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@
 6. lin_reg_loaded = joblib.load('lin_reg_model.joblib')
 7.
 8. # Judul aplikasi
-9. st.title (Prediksi Gaji Berdasarkan Lama Bekerja")
+9. st.title ("Prediksi Gaji Berdasarkan Lama Bekerja")
 10.
 11. # Input tahun pengalaman Kerja
 12. years_experience = st. number_input("Masukkan jumlah tahun bekerja:",min_value=0.0. step=0.1)
